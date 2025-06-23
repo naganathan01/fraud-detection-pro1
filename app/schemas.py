9bf6808d655b072cd7eeadf575acf596d249c954
naganathan01 <nathannathan42242@gmail.com>
2025-06-23 20:50:44 +0530

    based file structure

--- app/schemas.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class Transaction(BaseModel):
-    transaction_id: str
-    amount: float
-    location: str
-    device_type: str
-    is_foreign_transaction: bool
-    is_high_risk_country: bool
